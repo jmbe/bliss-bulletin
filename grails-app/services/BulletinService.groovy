2942b8f441a0b48bdaffd27d1a699885035a6415
Andreas Knifh <knifhen@knifhen-desktop.(none)>
2009-02-13 17:54:52 +0100

    removed use of java.sql.Blob

--- grails-app/services/BulletinService.groovy
+++ grails-app/services/BulletinService.groovy
@@ -10,13 +10,11 @@ class BulletinService {
 		//file.parentFile.mkdirs()
 		//downloadedfile.transferTo(file)
 		
-		def data = getDataBytes(Hibernate.createBlob(downloadedfile.inputStream))
+		def data = getDataBytes(downloadedfile.inputStream, (int)downloadedfile.size)
 		def bulletin = new Bulletin(name: filename, data: data).save()
     }
     
-	def getDataBytes(blob) {
-		def stream = blob.binaryStream
-		final int length = (int) blob.length()
+	def getDataBytes(stream, length) {
 		byte [] data = new byte [length]
 		if(stream.read(data, 0, length) < length) {
 			logger.error("n bytes read from stream where fewer than requested.")
