95838adac469f773a5a048f61fa854d97925f971
J-M Bergqvist <jm.bergqvis@gmail.com>
2009-04-06 20:00:58 +0200

    Use commons dbcp for development and production data sources.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,7 +1,20 @@
-// Place your Spring DSL code here
+import grails.util.Environment// Place your Spring DSL code here
 beans = {
 		authenticationEntryPoint(org.springframework.security.ui.basicauth.BasicProcessingFilterEntryPoint) {
 		    realmName = 'Bliss bulletin'
 		}
-
+		
+			if (["development", "production"].contains(Environment.current.name)) {						println("Using overridden datasource from resources.groovy.")
+		
+			dataSource(org.apache.commons.dbcp.BasicDataSource) { bean ->
+				bean.destroyMethod = "close"
+				
+				driverClassName="com.mysql.jdbc.Driver"
+				url="jdbc:mysql://localhost/bliss-bulletin"
+				username = "bulletin-dev"
+				password = "bulletin-dev"
+				testOnBorrow = "true"
+				validationQuery = "SELECT 1 FROM DUAL;"
+				defaultAutoCommit = "false"
+			}		}		
 }
