df14e2e6e9eaebe9df6638dc0c5aab40a79b7d0e
J-M Bergqvist <jm.bergqvist@gmail.com>
2011-12-10 19:30:50 +0100

    Create default role and admin.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,33 @@
 class BootStrap {
 
-	def authenticateService
+    def authenticateService
 
-	def init = { servletContext ->
-	}
-     
-	def destroy = {
-	}
-} 
+    def init = { servletContext ->
+        log.info "Init boot strap"
+        createRoles()
+        createDefaultUsers()
+    }
+
+    def createRoles = {
+        if (!Role.findByAuthority ("ROLE_ADMIN")) {
+            log.info "Creating admin role"
+            def role = new Role(authority : "ROLE_ADMIN", description: "Administrator")
+            if (!role.save()) {
+                role.errors.each(log.error(it) )
+            }
+        }
+    }
+
+    def createDefaultUsers = {
+        if(!Account.findByUsername("jmbe")) {
+            log.info "Adding initial admin"
+            def jmbe = new Account(username:"jmbe", email:"jm.bergqvist@gmail.com", userRealName: "J-M", enabled:true)
+            jmbe.passwd = authenticateService.encodePassword("nqqFjXK85U")
+
+            Role.findByAuthority ("ROLE_ADMIN").addToPeople(jmbe)
+        }
+    }
+
+    def destroy = {
+    }
+}
